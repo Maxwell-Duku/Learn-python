7a0cb425df624b2723d12e2d9569e4e8a594bd8d
Maxwell-Duku <maxiduku@gmail.com>
2023-10-31 21:13:29 +0000

    Boolean and Conditional Logic

---
+++ Boolean/Input.py
@@ -0,0 +1,15 @@
+#BOOLEAN AND CONDITIONAL LOGIC
+
+#Input function: prompt the use and store the result to a variable.
+
+data = input("what is your favoirte colour?")
+print("you said " + data)
+
+#the below codes print the result in a new line 
+#print("what is you favorite colour")
+#data = input()
+#print("you said " + data)
+
+
+
+
